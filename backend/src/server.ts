d69be6de68515a2d7769bd903f44652ab8fe8e8e
PrakatNeupane <neupaneprakat@gmail.com>
2024-07-16 00:01:53 +1000

    Remove node_modules from tracking

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,7 +7,7 @@ const port = env.PORT;
 mongoose
   .connect(process.env.MONGO_CONNECTION_STRING!)
   .then(() => {
-    console.log("Mongoose connected.");
+    console.log("Mongoose connected successfully.");
     app.listen(port, () => {
       console.log("Server running on port: " + port);
     });
