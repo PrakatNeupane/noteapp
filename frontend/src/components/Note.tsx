f133dcfeb9537ea873935fabd8460fa3a56f9700
PrakatNeupane <neupaneprakat@gmail.com>
2024-12-07 16:04:40 +1100

    showing date of update

--- frontend/src/components/Note.tsx
+++ frontend/src/components/Note.tsx
@@ -1,6 +1,7 @@
 import styles from "../styles/Note.module.css";
 import { Card } from "react-bootstrap";
 import { Note as NoteModel } from "../models/note";
+import { formatDate } from "../utils/formatDate";
 
 interface NoteProps {
   note: NoteModel;
@@ -9,12 +10,19 @@ interface NoteProps {
 
 const Note = ({ note }: NoteProps) => {
   const { title, text, createdAt, updatedAt } = note;
+
+  let createdUpdatedText: string;
+  if (updatedAt > createdAt) {
+    createdUpdatedText = "Updated: " + formatDate(updatedAt);
+  } else {
+    createdUpdatedText = "Created: " + formatDate(createdAt);
+  }
   return (
     <Card className={`${styles.noteCard}`}>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text className={styles.cardText}>{text}</Card.Text>
-        <Card.Footer className="text-muted">{createdAt}</Card.Footer>
+        <Card.Footer className="text-muted">{createdUpdatedText}</Card.Footer>
       </Card.Body>
     </Card>
   );
